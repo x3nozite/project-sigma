3cf9d2d1b69c21e43e7e907b2c6eb9d0da6a49f7
xenozite <xeno.zite3@gmail.com>
2025-09-28 19:03:42 +0700

    Refactor types to its own file

---
+++ frontend/src/components/types.tsx
@@ -0,0 +1,14 @@
+export type RectType = {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  id: string;
+  texts: TextType[]
+}
+export type TextType = {
+  id: string;
+  value: string;
+  fontSize: number;
+}
+

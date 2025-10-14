0e3a7195fe596ddbe673994bf08ad48d4fe737de
HmmmAha <hmmaha123456000@gmail.com>
2025-10-14 19:43:47 +0700

    deleted django, moving to supabase

--- backend/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from views import CreateUserView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-urlpatterns = [
-    path('register', CreateUserView.as_view(), name = "register")
-]

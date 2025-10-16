dab1c1cff926a3b87d345d3b6138ec4858207e61
xenozite <xeno.zite3@gmail.com>
2025-10-16 11:39:54 +0700

    Adjust rectangle's structure

--- frontend/src/components/Rectangle.tsx
+++ frontend/src/components/Rectangle.tsx
@@ -39,19 +39,87 @@ const Rectangle = ({
       onDragEnd={onDragEnd}
       onClick={() => handleClick(rect.id)}
     >
-      <Rect
-        id={rect.id}
-        width={Math.round(rect.width)}
-        height={Math.round(rect.height)}
-        fill="white"
-        shadowBlur={10}
-        shadowOpacity={0.5}
-        shadowColor="black"
-        shadowOffset={{ x: 0, y: 4 }}
-        cornerRadius={[16, 4, 4, 4]}
-        isCollapsed={false}
-      />
+      <Group
+        visible={rect.isCollapsed}
+      >
+        <Rect
+          id={rect.id}
+          width={Math.round(rect.width)}
+          height={Math.round(rect.height)}
+          fill="white"
+          shadowBlur={10}
+          shadowOpacity={0.5}
+          shadowColor="black"
+          shadowOffset={{ x: 0, y: 4 }}
+          cornerRadius={[16, 4, 4, 4]}
+        />
+        <Text
+          x={0}
+          y={50}
+          text={rect.description}
+          fontSize={14}
+          fontFamily="Inter"
+          fontStyle="normal"
+          fill="gray"
+          align="justify"
+          ellipsis={true}
+          lineHeight={1.25}
+          padding={10}
+          width={rect.width}
+          height={rect.height}
+          listening={false}
+        />
+        <Text
+          text={rect.dueDate}
+          x={0}
+          y={rect.height - 35}
+          width={rect.width}
+          height={rect.height}
+          fontFamily="Inter"
+          fontSize={12}
+          fontStyle="light"
+          padding={5}
+          align="right"
+          fill="blue"
+          opacity={0.8}
+          listening={false}
+        ></Text>
+        <Group>
+          <Rect
+            fill="#efb100"
+            width={100}
+            height={30}
+            x={10}
+            y={rect.height - 40}
+            cornerRadius={10}
+          ></Rect>
+          <Text
+            text={rect.status}
+            width={100}
+            height={30}
+            fontFamily="Inter"
+            align="left"
+            fill="white"
+            x={17}
+            y={rect.height - 42}
+            padding={10}
+          ></Text>
+        </Group>
+      </Group>
       <Group>
+        <Rect
+          x={0}
+          y={0}
+          width={rect.width}
+          height={rect.height * 0.25}
+          cornerRadius={[16, 4, 4, 4]}
+          fill={rect.color}
+          shadowBlur={10}
+          shadowOpacity={0.5}
+          shadowColor="black"
+          shadowOffset={{ x: 0, y: 4 }}
+          visible={!rect.isCollapsed}
+        />
         <Rect
           x={0}
           y={0}
@@ -91,60 +159,6 @@ const Rectangle = ({
               });
             }
           }}
-        ></Text>
-      </Group>
-      <Group>
-        <Text
-          x={0}
-          y={50}
-          text={rect.description}
-          fontSize={14}
-          fontFamily="Inter"
-          fontStyle="normal"
-          fill="gray"
-          align="justify"
-          ellipsis={true}
-          lineHeight={1.25}
-          padding={10}
-          width={rect.width}
-          height={rect.height}
-          listening={false}
-        />
-      </Group>
-      <Text
-        text={rect.dueDate}
-        x={0}
-        y={rect.height - 35}
-        width={rect.width}
-        height={rect.height}
-        fontFamily="Inter"
-        fontSize={12}
-        fontStyle="light"
-        padding={5}
-        align="right"
-        fill="blue"
-        opacity={0.8}
-        listening={false}
-      ></Text>
-      <Group>
-        <Rect
-          fill="#efb100"
-          width={100}
-          height={30}
-          x={10}
-          y={rect.height - 40}
-          cornerRadius={10}
-        ></Rect>
-        <Text
-          text={rect.status}
-          width={100}
-          height={30}
-          fontFamily="Inter"
-          align="left"
-          fill="white"
-          x={17}
-          y={rect.height - 42}
-          padding={10}
         ></Text>
       </Group>
     </Group>

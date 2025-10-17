f4d8164a44648496da287210a6a1657f47b8d64f
farrelpit <farrelputra141@gmail.com>
2025-10-17 21:55:30 +0700

    login and sign in, no tables yet so no pp and username based login for now

--- frontend/src/components/CreateAccountPage.tsx
+++ frontend/src/components/CreateAccountPage.tsx
@@ -1,8 +1,49 @@
 import { useNavigate } from "react-router-dom";
 import { SecondButton } from "./ui/buttons";
+import { createClient } from "@supabase/supabase-js";
+import { useState } from "react";
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
 
 function CreateAccount() {
     const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [username, setUsername] = useState("");
+
+    const handleCreateAccount = async () => {
+        if (password !== confirmPassword) {
+            alert("Passwords do not match!");
+            return;
+        }
+        
+        try {
+            const {data, error} = await supabase.auth.signUp({
+                email,
+                password,
+                options: {
+                    data: {
+                        username,
+                    },
+                },
+            });
+
+            if (error) {
+                alert("Error creating account: " + error.message);
+            } else {
+                alert("Account created successfully, please verify your email");
+                navigate("/signin");
+            }
+        }
+
+        catch (error) {
+            alert("Error creating account: " + error);
+        }
+
+
+    }; 
+    
     return (
         <div>
             createacc
--- frontend/src/components/SignInPage.tsx
+++ frontend/src/components/SignInPage.tsx
@@ -1,53 +1,86 @@
 import { useNavigate } from "react-router-dom";
 import { SecondButton } from "./ui/buttons";
+import { createClient } from "@supabase/supabase-js";
+import { useState } from "react";
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
 
 function SignIn() {
     const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSignIn = async () => {
+        try {
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email,
+            password,
+        });
+
+        if (error) {
+            alert(error.message);
+            return;
+        }
+
+        alert("Login successful!");
+        navigate("/");
+        } catch (error) {
+        console.error(error);
+        alert("Something went wrong!");
+        }
+    };
     return (
-        <div className="flex min-h-screen items-center justify-center bg-gray-100">
-            <div className="w-full max-w-md bg-white rounded-lg shadow-md p-8">
-                <h2 className="text-2xl text-purple-700 font-bold mb-6 text-center">Sign In</h2>
-                <form className="space-y-5">
-                    <div>
-                        <label className="block text-sm text-purple-700 font-medium mb-1" htmlFor="email">
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            placeholder="Enter your email"
+
+        <div className="">
+            <div className="flex min-h-screen items-center justify-center bg-gray-100">
+                <div className="w-full max-w-md bg-white rounded-lg shadow-md p-8">
+                    <h2 className="text-2xl text-purple-700 font-bold mb-6 text-center">Sign In</h2>
+                    <form className="space-y-5">
+                        <div>
+                            <label className="block text-sm text-purple-700 font-medium mb-1" htmlFor="email">
+                                Email
+                            </label>
+                            <input
+                                type="email"
+                                id="email"
+                                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                placeholder="Enter your email"
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <label className="block text-sm text-purple-700 font-medium mb-1" htmlFor="password">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                id="password"
+                                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                placeholder="Enter your password"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <div className="flex justify-center">
+                            <SecondButton
+                            title="Sign In"
+                            variant="invert"
+                            onClick={handleSignIn}
+                            />
+                        </div>
+                        
+                    </form>
+                    <div className="mt-6 flex justify-center">
+                        <SecondButton 
+                        title="Back" 
+                        onClick={() => navigate("/")} 
                         />
                     </div>
-                    <div>
-                        <label className="block text-sm text-purple-700 font-medium mb-1" htmlFor="password">
-                            Password
-                        </label>
-                        <input
-                            type="password"
-                            id="password"
-                            className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            placeholder="Enter your password"
-                        />
-                    </div>
-                    <div className="flex justify-center">
-                        <SecondButton
-                        title="Sign In"
-                        variant="invert"
-                        onClick={(e) => { e.preventDefault();}}
-                        />
-                    </div>
-                    
-                </form>
-                <div className="mt-6 flex justify-center">
-                    <SecondButton 
-                    title="Back" 
-                    onClick={() => navigate("/")} 
-                    />
                 </div>
             </div>
         </div>
     );
+        
+            
 }
 
 export default SignIn;

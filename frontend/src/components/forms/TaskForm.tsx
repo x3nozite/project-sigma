35889c3be39b93854113461fdb620944934cab1a
Briant-s <briantsandriano@gmail.com>
2025-10-17 12:19:42 +0700

    kinda cleaned up the taskform

--- frontend/src/components/forms/TaskForm.tsx
+++ frontend/src/components/forms/TaskForm.tsx
@@ -33,8 +33,8 @@ export default function TaskForm({ onAddTask, onCloseForm }: Props) {
     formState: { errors, isSubmitting },
   } = useForm<taskFields>({
     defaultValues: {
-      title: "abc",
-      description: "abcedfghijkl",
+      title: "My New Subject",
+      // description: "abcedfghijkl",
       date: "2025-10-11",
       time: "06:07",
     },
@@ -56,49 +56,64 @@ export default function TaskForm({ onAddTask, onCloseForm }: Props) {
   };
 
   return (
-    <div className="absolute bg-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 px-10 py-10 inset-0 flex flex-col gap-4 items-center justify-center w-fit h-fit border-px rounded-lg shadow-2xl z-100">
+    <div className="absolute bg-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 px-10 py-10 inset-0 flex flex-col justify-between w-150 h-fit max-h-200  items-start border-px rounded-lg shadow-2xl z-100">
       <div
-        className="absolute top-0 rounded-t-lg w-full h-5 "
+        className="absolute top-0 left-0 rounded-t-lg w-full h-5 "
         style={{ backgroundColor: selCol }}
       ></div>
-      <div className="flex flex-row justify-between w-full h-full">
-        <span className="text-2xl font-bold text-left">Create New Task</span>
-        <button onClick={onCloseForm} className="hover:cursor-pointer">
-          X
-        </button>
-      </div>
-      <hr className="h-px w-full my-2 bg-gray-200 border-0" />
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-5 flex flex-col gap-1">
-          <label className="text-lg font-semibold">Task Title</label>
+      <form onSubmit={handleSubmit(onSubmit)} className="w-full h-full">
+        <div className="mb-2 flex flex-row items-end justify-between h-15  w-full">
           <input
             {...register("title")}
             type="text"
             id="title"
-            placeholder="My New Task"
-            className="border border-gray-100 bg-gray-50 rounded-lg p-2 shadow-xs"
-          />
+            size={11}
+            placeholder="My New Subject"
+            className="text-3xl h-full p-2 font-bold rounded-lg"
+          ></input>
           {errors.title && (
             <div className="text-red-500">{errors.title?.message}</div>
           )}
+          <button
+            onClick={onCloseForm}
+            className="hover:cursor-pointer p-2 h-full"
+          >
+            X
+          </button>
         </div>
-        <div className="mb-5 flex flex-col gap-1">
-          <div className="inline-flex ">
-            <label className="text-lg font-semibold">Description</label>
+        <div className="mb-8 flex flex-col gap-5 w-full items-start">
+          <div className="inline-flex gap-2">
+            <svg
+              className="w-6 h-6 text-gray-800"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="12"
+              height="12"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-width="2"
+                d="M5 7h14M5 12h14M5 17h10"
+              />
+            </svg>
+
+            <label className="text-md font-semibold">Description</label>
           </div>
-          <input
+          <textarea
             {...register("description")}
-            type="text"
-            placeholder="This is the description for my task!"
-            className="border border-gray-100 bg-gray-50 rounded-lg p-2 shadow-xs"
-          />
+            placeholder="Add a detailed description for subject..."
+            className="border border-gray-300 bg-gray-50 rounded-lg p-2 shadow-xs ml-8 w-[calc(100%-2rem)] items-start h-25 resize-none"
+          ></textarea>
           {errors.description && (
             <div className="text-red-500">{errors.description?.message}</div>
           )}
         </div>
         <hr className="h-px w-full my-2 bg-gray-200 border-0" />
-        <div className="flex flex-row flex-auto gap-10">
-          <div className="mb-5 flex flex-col gap-1">
+        <div className="flex flex-row flex-auto gap-10 w-full mt-5 mb-5">
+          <div className="mb-5 flex flex-col gap-1 w-full">
             <label className="text-lg font-semibold">Date</label>
             <input
               {...register("date")}
@@ -133,19 +148,19 @@ export default function TaskForm({ onAddTask, onCloseForm }: Props) {
           </div>
         </div>
 
-        <div className="flex flex-row justify-start gap-2 w-full h-full">
+        <div className="flex flex-row justify-between gap-2 w-full h-full">
           <button
-            type="submit"
-            disabled={isSubmitting}
-            className="py-2.5 px-5 text-md font-semibold rounded-lg text-white bg-amber-500 hover:bg-amber-600 hover:cursor-pointer"
-          >
-            {isSubmitting ? "Loading..." : "Create New Task"}
-          </button>
-          <button
-            className="border border-red-500 font-medium text-red-500 bg-red-50 px-5 rounded-lg hover:bg-red-500 hover:text-white hover:cursor-pointer"
+            className="border py-2.5 px-5 h-fit border-red-500 font-medium text-red-500 bg-red-50 px-5 rounded-lg hover:bg-red-500 hover:text-white hover:cursor-pointer"
             onClick={onCloseForm}
           >
             Cancel
+          </button>
+          <button
+            type="submit"
+            disabled={isSubmitting}
+            className="py-2.5 px-5 h-fit text-md font-semibold rounded-lg text-white bg-amber-500 hover:bg-amber-600 hover:cursor-pointer"
+          >
+            {isSubmitting ? "Loading..." : "Create New Task"}
           </button>
         </div>
         {errors.root && (

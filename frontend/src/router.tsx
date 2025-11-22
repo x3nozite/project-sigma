e9a68d3123695e49310f71c71df8ed265bacc82e
xenozite <xeno.zite3@gmail.com>
2025-11-22 15:06:46 +0700

    change createBrowserRouter to createHashRouter

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,12 +1,12 @@
 import SignIn from "./components/SignInPage";
 import CreateAccount from "./components/CreateAccountPage";
 import App from "./App";
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 
 
 
-export const router = createBrowserRouter([
-    { path: "/", element: <App /> },
-    { path: "/signin", element: <SignIn /> },
-    { path: "/create-acc", element: <CreateAccount /> },
+export const router = createHashRouter([
+  { path: "/", element: <App /> },
+  { path: "/signin", element: <SignIn /> },
+  { path: "/create-acc", element: <CreateAccount /> },
 ]);

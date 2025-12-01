74cb2c48955eeb3cb33228b9e3742bb1b9c0fd27
HmmmAha <hmmaha123456000@gmail.com>
2025-12-01 09:36:26 +0700

    added validation for username and email

--- frontend/src/components/CreateAccountPage.tsx
+++ frontend/src/components/CreateAccountPage.tsx
@@ -29,6 +29,26 @@ const schema = z
 
 export type userFields = z.infer<typeof schema>;
 
+export async function checkEmailExists(email: string) {
+  const { data } = await supabase
+    .from("users")
+    .select("id")
+    .eq("email", email)
+    .single();
+
+  return !!data; // converts to boolean (true if exists)
+}
+
+export async function checkUsernameExists(username: string) {
+  const { data } = await supabase
+    .from("users")
+    .select("id")
+    .eq("username", username)
+    .single();
+
+  return !!data; // converts to boolean (true if exists)
+}
+
 function CreateAccount() {
   const {
     register,

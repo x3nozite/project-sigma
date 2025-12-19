c1cc25bad11966de351e82e70cbc9baa54754bd5
HmmmAha <hmmaha123456000@gmail.com>
2025-12-19 11:51:07 +0700

    silghtly improved fetching time

--- frontend/src/context/SessionContext.tsx
+++ frontend/src/context/SessionContext.tsx
@@ -14,16 +14,14 @@ export function SessionProvider({ children }: { children: React.ReactNode }) {
   const [session, setSession] = useState<any | null>(null);
 
   useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {   
-      setSession(session);
-    });
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-    return () => subscription.unsubscribe();
-  }, []);
+  const {
+    data: { subscription },
+  } = supabase.auth.onAuthStateChange((_event, session) => {
+    setSession(session);
+  });
+
+  return () => subscription.unsubscribe();
+}, []);
 
   return (
     <SessionContext.Provider value={{ session, setSession }}>
